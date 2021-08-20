1631952f5e827acc84d436267dac2fde2c7cce24
Fabian Duran <ingfabianavellaneda@gmail.com>
2021-08-19 19:37:00 -0500

    Se termina de maquetar el frontend

--- assets/js/boardPlayer.js
+++ assets/js/boardPlayer.js
@@ -3,11 +3,19 @@ const drawBoardPlayer = (list) => {
     for (let i = 0; i < list.length; i ++) {
         cards += `<div class="col-3">
                     <div 
-                        class="card mb-2 container__board--card">
+                        class="card mb-2 container__board--card border-0">
                         <img 
                             src="${(list[i].isDiscover) ? list[i].image : './assets/images/question_icon.png'}"
-                            class="container__board--images"
+                            class="card-img-top"
                             alt="Image question">
+                            <div 
+                                class="card-body p-0 h-100 d-flex flex-column justify-content-center ">
+                                ${(list[i].isDiscover) ? '<h5 class="card-title card--title">${list[i].value}</h5>' : ''}
+                                <button class="btn button--main btn-sm">
+                                    <i class="bi bi-eye-fill"></i>
+                                    Ver
+                                </button>
+                          </div>
                     </div>
                 </div>`; 
     }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const main = async () => {
     const BOARD_PLAYER = document.getElementById('board');
-    const LIST_BOARD = await getListBoard(20);
+    const LIST_BOARD = await getListBoard(12);
     BOARD_PLAYER.innerHTML = drawBoardPlayer(LIST_BOARD);
 };
 
