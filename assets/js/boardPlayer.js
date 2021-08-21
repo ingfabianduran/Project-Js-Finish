2b5b48746a9b4275f65f148b0990444cc112d542
Fabian Duran <ingfabianavellaneda@gmail.com>
2021-08-20 20:55:43 -0500

    Se crea la logica de registro

--- assets/js/boardPlayer.js
+++ assets/js/boardPlayer.js
@@ -1,21 +1,21 @@
 const drawBoardPlayer = (list) => {
     let cards = `<div class="row">`;
     for (let i = 0; i < list.length; i ++) {
-        cards += `<div class="col-3">
+        cards += `<div class="col-3 d-flex justify-content-center mb-2">
                     <div 
-                        class="card mb-2 container__board--card border-0">
+                        class="card h-100 mb-2 container__board--card border-0">
                         <img 
                             src="${(list[i].isDiscover) ? list[i].image : './assets/images/question_icon.png'}"
                             class="card-img-top"
                             alt="Image question">
-                            <div 
-                                class="card-body p-0 h-100 d-flex flex-column justify-content-center ">
-                                ${(list[i].isDiscover) ? '<h5 class="card-title card--title">${list[i].value}</h5>' : ''}
-                                <button class="btn button--main btn-sm">
-                                    <i class="bi bi-eye-fill"></i>
-                                    Ver
-                                </button>
-                          </div>
+                        <div 
+                            class="card-body p-0 d-flex flex-column justify-content-center ">
+                            ${(list[i].isDiscover) ? `<h5 class="card-title card--title">${list[i].value}</h5>` : '<h5 class="card-title card--title">???</h5>'}
+                            <button class="btn button--main btn-sm">
+                                <i class="bi bi-eye-fill"></i>
+                                Ver
+                            </button>
+                        </div>
                     </div>
                 </div>`; 
     }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,31 @@
-const main = async () => {
+const submitForm = () => {
+    const FORM_PLAYER = document.getElementById('formPlayer');
+    const MODAL_FORM = new bootstrap.Modal(document.getElementById('formModal'), {
+        backdrop: 'static',
+        keyboard: false
+    });
+    MODAL_FORM.show();
+    FORM_PLAYER.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const FORM_DATA = new FormData(FORM_PLAYER);
+        const BUTTON_SUBMIT = document.getElementById('buttonSubmit');
+        const LEVEL = parseInt(FORM_DATA.get('dificulty'));
+        BUTTON_SUBMIT.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
+        localStorage.setItem('namePlayer', FORM_DATA.get('namePlayer'));
+        localStorage.setItem('numBusqueda', 0);
+        setTimeout(() => {
+            MODAL_FORM.hide();
+            initPlayer(LEVEL);
+        }, 3000);
+    });
+}; 
+
+const initPlayer = async (dificulty) => {
     const BOARD_PLAYER = document.getElementById('board');
-    const LIST_BOARD = await getListBoard(12);
+    const LIST_BOARD = await getListBoard(dificulty);
     BOARD_PLAYER.innerHTML = drawBoardPlayer(LIST_BOARD);
+    BOARD_PLAYER.classList.remove('d-none');
+    localStorage.setItem('boardMachine', JSON.stringify(LIST_BOARD));
 };
 
-main();
+submitForm();
